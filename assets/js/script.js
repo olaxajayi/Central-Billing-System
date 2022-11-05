fde4bf9c189b97b52cca55f3e68978b3d9018a1d
olaxajayi <62068794+olaxajayi@users.noreply.github.com>
2022-11-05 22:50:22 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@
 
 
 
+var signInButton = document.querySelector('.sign-in-button');
 
 var hamburger = document.querySelector('.hamburger');
 var closeButton = document.querySelector('.close-button');
@@ -48,6 +49,44 @@ var searchInvoice = document.querySelector('.search-invoice');
 var expandMoreTenant = document.querySelector('.expand-more-tenant');
 var expandLessTenant = document.querySelector('.expand-less-tenant');
 var tenantOptions = document.getElementById('#tenant-sub-options');
+
+
+
+
+
+
+signInButton.addEventListener('click', function(){
+  var usernameInputValue = document.querySelector('.username-input').value;
+  var passwordInputValue = document.querySelector('.password-input').value;
+  var usernameError = document.querySelector('.username-error');
+  var passwordError = document.querySelector('.password-error');
+  var linkToDashboard = document.querySelector('.link-to-dashboard');
+
+  if (usernameInputValue.trim().length > 0 && passwordInputValue.trim().length < 6) {
+    usernameError.style.display = "none";
+    passwordError.style.display = "block";
+  }
+
+  else if (usernameInputValue.trim().length === 0 && passwordInputValue.trim().length >= 6) {
+    usernameError.style.display = "block";
+    passwordError.style.display = "none";
+  }
+
+  else if (usernameInputValue.trim().length === 0 && passwordInputValue.trim().length < 6) {
+    usernameError.style.display = "block";
+    passwordError.style.display = "block";
+  }
+
+  else if (usernameInputValue.trim().length > 0 && passwordInputValue.trim().length >= 6) {
+    linkToDashboard.href = "dashboard.html";
+    usernameError.style.display = "none";
+    passwordError.style.display = "none";
+  }
+
+  else{
+    console.log("shh");
+  }
+});
 
 
 hamburger.addEventListener('click', function(){
